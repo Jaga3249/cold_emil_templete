79baa0985e937e1c00c42afddcb3b1ed50876fd8
jagannathsde-coder <jagannathsde@pgagi.in>
2025-09-05 11:43:36 +0530

    modify the last line

--- src/components/EmailGenerator.jsx
+++ src/components/EmailGenerator.jsx
@@ -36,9 +36,11 @@ I hope you're doing well. My name is <b>Jagannath Behera</b>, and I'm reaching o
 I have <b>2 year</b> of hands-on experience in frontend development, specializing in <b>React.js, Next.js, React Native, TypeScript, and Tailwind CSS</b>, with a foundational understanding of <b>Node.js</b>. I’ve worked on projects focused on building intuitive, responsive user interfaces and integrating modern web functionalities.<br/><br/>
 
 
+I’ve attached my <a href="https://drive.google.com/file/d/1IA6jb0eznoKvol9IqCm1PGW9OO-HbHVk/view" target="_blank" title="Resume">Resume</a> for your review.  
+If my profile aligns with your requirements, I would be grateful for an opportunity to interview with <b>${companyName}</b>.  
+<br/><br/>
 
 
-I’ve attached <a href="https://drive.google.com/file/d/1IA6jb0eznoKvol9IqCm1PGW9OO-HbHVk/view" target="_blank" title="https://drive.google.com/file/d/1IA6jb0eznoKvol9IqCm1PGW9OO-HbHVk/view">View Resume</a> for you to take a look at. If you find me suitable, please help me with an Interview Opportunity at <b>${companyName}</b> <br/><br/>
 
 
 Best regards,<br/>
